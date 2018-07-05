c2c987727db6cd2b9a71e4e7e1347f56f60dac28
HiDana <holicdana@gmail.com>
2018-07-05 11:24:49 +0800

    Fixed firefox login bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -69,6 +69,7 @@ class Header extends Component {
     const oldEditFishInfo = this.state.editFishInfo;
     const { newEditFishInfo } = nextProps;
 
+		//監聽修改魚的資訊
     if (newEditFishInfo !== oldEditFishInfo) {
       const newNode = {
         name: newEditFishInfo.key,
